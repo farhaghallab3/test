2367114e6b5a8c3de223c4acd4d7f296b3f03cc2
the-aphrodite <128906057+the-aphrodite@users.noreply.github.com>
2024-03-09 16:07:04 +0200

    Add files via upload

---
+++ lib/Domain/themeNotifier.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ThemeNotifier extends ChangeNotifier {
+  bool _isDarkModeEnabled = false;
+
+  bool get isDarkModeEnabled => _isDarkModeEnabled;
+
+  void toggleDarkMode(bool value) {
+    _isDarkModeEnabled = value;
+    notifyListeners();
+  }
+}
